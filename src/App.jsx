1b0817b32916b14e15171804ce10e3ea2969f7db
Hussain Intzar <butthussain2@gmail.com>
2024-09-05 21:10:24 +0500

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import SignUp from "./pages/SignUp";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
