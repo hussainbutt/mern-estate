0693d3a73d889e7e527ecaf4e731affb53570429
Hussain Intzar <butthussain2@gmail.com>
2024-10-13 19:31:07 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to DB");
+}).catch((err) => {
+    console.log("ERROR WHILE CONNECTING TO DB");
+});
+
 
 const app = express();
+app.get('/', (req, res) => {
+    res.send('GET request to the homepage')
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
