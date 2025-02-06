d9f2960733d9a65f60eeeaed730dbf4158f1e413
Hussain Intzar <butthussain2@gmail.com>
2025-02-06 23:45:56 +0500

    Complete Profile Page UI

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -4,5 +4,5 @@ import { useSelector } from "react-redux";
 
 export default function PrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
-  return currentUser ? <Outlet /> : <Navigate to="/signin" />;
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
 }
