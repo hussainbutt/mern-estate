73e5f1181e4ca8f0cd62ce55ad704f47e9d5b47b
Hussain Intzar <butthussain2@gmail.com>
2025-03-07 16:19:14 +0500

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,13 +5,14 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js'
+import path from "path";
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to DB");
 }).catch((err) => {
     console.log("ERROR WHILE CONNECTING TO DB");
 });
-
+const _dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -32,6 +33,11 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'));
+});
 //Lets make a middleware for error handling
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
