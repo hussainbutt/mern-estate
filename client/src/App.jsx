f7c9275faa7ee84fa5f2383109affef08f8e2264
Hussain Intzar <butthussain2@gmail.com>
2025-03-07 16:42:43 +0500

    project complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import SignUp from "./pages/SignUp";
-import Header from "./components/header";
+import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
